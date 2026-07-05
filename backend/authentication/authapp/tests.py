d222b2d950df834707cee76a9e88dd06614614f5
izadbakhshpadmira <izadbakhsh.p16@gmail.com>
2026-07-05 01:16:36 -0400

    Added test functions for linking performer and manager

--- backend/authentication/authapp/tests.py
+++ backend/authentication/authapp/tests.py
@@ -3,7 +3,7 @@ from django.contrib.sessions.middleware import SessionMiddleware
 from django.core import mail
 from unittest.mock import patch
 
-from .models import Users, Roles
+from .models import Users, Roles, PerformerLinks, PerformerLinkRequests
 from .backend import (
     email_exists,
     pass_hash,
@@ -14,9 +14,10 @@ from .backend import (
     get_user,
     verify,
     authenticate_user,
-    login_user,
-    get_loggedin_user,
-    logout_user
+    request_manager_link,
+    get_pending_manager_requests,
+    approve_manager_request,
+    deny_manager_request,
 )
 # Create your tests here.
 
@@ -32,6 +33,26 @@ class Authentication_tests(TestCase):
             role = self.role,
             verified = True
             )
+        
+        self.manager_role = Roles.objects.get(role_name="manager")
+        self.manager = Users.objects.create(
+            forename="manager",
+            surname="user",
+            email="manager@mylaurier.ca",
+            password_hash=pass_hash("ManagerUser!"),
+            role=self.manager_role,
+            verified=True
+        )
+
+        self.performer_role = Roles.objects.get(role_name="performer")
+        self.performer = Users.objects.create(
+            forename="performer",
+            surname="user",
+            email="performer@mylaurier.ca",
+            password_hash=pass_hash("PerformerUser!"),
+            role=self.performer_role,
+            verified=True
+        )
         
     def test_email_exists(self):
         self.assertTrue(email_exists("test@mylaurier.ca"))
@@ -103,3 +124,58 @@ class Authentication_tests(TestCase):
         done, message = authenticate_user("auth@mylaurier.ca", "ThirdTestPassword!")
         self.assertTrue(done)
         self.assertIsNotNone(user)
+    
+    def test_request_manager_link(self):
+        success, request = request_manager_link(self.performer, self.manager)
+        self.assertTrue(success)
+        self.assertEqual(request.status, "pending")
+        self.assertTrue(PerformerLinkRequests.objects.filter(performer=self.performer, manager=self.manager
+            ).exists())
+        
+    def test_request_manager_link_twice(self):
+        request_manager_link(self.performer, self.manager)
+        success, message = request_manager_link(self.performer, self.manager)
+        self.assertFalse(success)
+        self.assertEqual(message, "A request has already been sent.")
+
+    def test_get_pending_manager_requests(self):
+        request_manager_link(self.performer, self.manager)
+        requests = get_pending_manager_requests(self.manager)
+        self.assertEqual(len(requests), 1)
+        self.assertEqual(requests[0].performer.email,"performer@mylaurier.ca")
+
+    def test_approve_manager_request(self):
+        success, request = request_manager_link(self.performer, self.manager)
+        done, message = approve_manager_request(self.manager, request.request_id)
+        self.assertTrue(done)
+        self.assertEqual(message, "Approved")
+        self.assertTrue(PerformerLinks.objects.filter(performer=self.performer, manager=self.manager
+            ).exists())
+        request.refresh_from_db()
+        self.assertEqual(request.status, "approved")
+
+    def test_deny_manager_request(self):
+        success, request = request_manager_link(self.performer, self.manager)
+        done, message = deny_manager_request(self.manager,request.request_id)
+        self.assertTrue(done)
+        self.assertEqual(message, "Denied")
+        request.refresh_from_db()
+        self.assertEqual(request.status, "denied")
+        self.assertFalse(PerformerLinks.objects.filter(performer=self.performer,manager=self.manager
+            ).exists())
+        
+    def test_approve_invalid_request(self):
+        done, message = approve_manager_request(self.manager, 2026)
+        self.assertFalse(done)
+        self.assertEqual(message, "Request not found.")
+
+    def test_deny_invalid_request(self):
+        done, message = deny_manager_request(self.manager, 2026)
+        self.assertFalse(done)
+        self.assertEqual(message, "Request not found.")
+
+    def test_non_manager_cannot_approve(self):
+        success, request = request_manager_link(self.performer, self.manager)
+        done, message = approve_manager_request(self.performer, request.request_id)
+        self.assertFalse(done)
+        self.assertEqual(message, "User is not a manager.")
